16d8fda32957045a590389e3be91ad271e1c4c4e
Drake Morrison <drakermorrison@gmail.com>
2018-03-08 14:11:05 -0600

    added blogs.js and updated index.html, updated navbar across site to include blogs.html

---
+++ javascripts/blogs.js
@@ -0,0 +1,20 @@
+var blogs = [
+    {
+        id: "blog1", 
+        title: "The Journey Begins", 
+        date: "02/22/2018", 
+        post: "Starting this course I thought I had a basic knowledge of HTML and CSS, and to a point, I did. However the picture I had was incomplete; it was missing crucial pieces of design and syntax to the languages that I had never heard of before. I learned right from the get-go that coding was a team sport, and it was much more fun that way. Having a team forces one to organize their code so that it becomes easily readable to your teammates, and in the process, it becomes more readable to yourself as well. Semantic markup was a great tool for accomplishing this in HTML. The ability to replace a bunch of div's and comments into a single article, section, or aside tag immensely cleans up code. However in CSS I am still using comments to organize sections of styling. I have recently learned about CSS variables, but I don't have enough practice with them to tell if they really help this problem the same way Semantic Markup helps HTML code. GIT: What is it? What does it do? Why bother? The answers to all these questions and more I encountered this week at NSS. I am learning to love git for it's ability to organize code into commits and branches. I love being able to work without fear of breaking something and ruining my site. In the same vein, learning the Command Line has been a very useful tool. It feels great to type in a single command to complete several tasks at once. Overall, I have learned more than I thought I would in the first couple of weeks here at NSS, and I can barely wait to learn more!"
+    },
+    {
+        id: "blog2",
+        title: "Week 3:Basic JavaScript",
+        date: "03/01/2018",
+        post: "I am super excited to start learning JavaScript. After doing the Treehouse course and especially 'A Smarter Way to Learn JavaScript' by Mark Myers, I was well prepared for the coursework this week. We went over basic data types and array methods. It was fun; can't wait for more!"
+    },
+    {
+        id: "blog3",
+        title: "Week 4:Advanced JS and Git for Teams",
+        date: "03/08/2018",
+        post: "This week was fun. We learned about using GitHub projects to organize a project and break it down into manageable chunks that each team member can code. We got to practice at our weekly study group. We had homework on Functions and Objects that seemed a lot easier than the previous homework so far. It's really cool to see how I can code more complex projects as my skills improve. That's all for now."
+    },
+  ];
