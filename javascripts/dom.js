29fcfd352fa39852453a5ddbedcd384567fcab77
Drake Morrison <drakermorrison@gmail.com>
2018-06-09 11:43:24 -0500

    changed projects and homepage;

--- javascripts/dom.js
+++ javascripts/dom.js
@@ -4,10 +4,12 @@ function createProjectCards (inputArray) {
   let domString = '';
   inputArray.forEach((e) => {
     domString += '<article>';
-    domString +=    `<img src="${e.imageUrl}">`;
+    domString +=    `<img src="${e.thumbnail}" class='img-thumbnail img-responsive'>`;
     domString +=    `<h2>${e.title}</h2>`;
     domString +=    `<p>${e.description}</p>`;
-    domString +=    `<a href="${e.githubUrl}">See it in Github</a>`;
+    domString +=    `<p>Technologies Used: ${e.technologiesUsed}</p>`;
+    domString +=    `<a href="${e.github}" target='_blank'>See it in Github</a>`;
+    domString +=    `<a href='${e.url}' target="_blank">Go to Project Page</a>`;
     domString += `</article>`;
   });
   writeToDom(domString, '#my-projects');
